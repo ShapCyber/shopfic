c54f8377dda5587833a412bccd8fe1b85d0bc824
ShapCyber <adego247@gmail.com>
2016-11-24 19:11:32 +0000

    Create shopfy.js

---
+++ shopfy.js
@@ -0,0 +1,147 @@
+(function() {
+
+	// getElementById
+	function GetElementById(id) {
+		return document.getElementById(id);
+	}
+
+
+	// output information
+	function Output(msg) {
+		var m = GetElementById("messages");
+		m.innerHTML = msg + m.innerHTML;
+	}
+
+
+	// file drag hover
+	function FileDragHover(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.target.className = (e.type == "dragover" ? "hover" : "");
+	}
+
+
+	// file selection
+	function FileSelectHandler(e) {
+
+		// cancel event and hover styling
+		FileDragHover(e);
+
+		// fetch FileList object
+		var files = e.target.files || e.dataTransfer.files;
+
+		// process all File objects
+		for (var i = 0, fx; fx = files[i]; i++) {
+			ParseFile(fx);
+			UploadFile(fx);
+		}
+
+	}
+
+
+	// output file information
+	function ParseFile(file) {
+
+		/*Output(
+			"<p>File information: <strong>" + file.name +
+			"</strong> type: <strong>" + file.type +
+			"</strong> size: <strong>" + file.size +
+			"</strong> bytes</p>"
+		);*/
+
+		// display an image
+		if (file.type.indexOf("image") == 0) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				Output(
+					"<p><img width=100 src=" + e.target.result + " /></p>"
+				);
+			}
+			reader.readAsDataURL(file);
+			
+		}
+
+		// display text
+		if (file.type.indexOf("text") == 0) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				Output(
+					"<p><strong>" + file.name + ":</strong></p><pre>" +
+					e.target.result.replace(/</g, "&lt;").replace(/>/g, "&gt;") +
+					"</pre>"
+				);
+			}
+			reader.readAsText(file);
+		}
+
+	}
+
+
+	// upload JPEG files
+	function UploadFile(file) {		
+
+		var xhr = new XMLHttpRequest();
+		if (xhr.upload && file.type == "image/jpeg" && file.size <= GetElementById("MAX_FILE_SIZE").value) {
+
+			// create progress bar
+			var o = GetElementById("progress");
+			var progress = o.appendChild(document.createElement("p"));
+			progress.appendChild(document.createTextNode("upload " + file.name));
+
+
+			// progress bar
+			xhr.upload.addEventListener("progress", function(e) {
+				var pc = parseInt(100 - (e.loaded / e.total * 100));
+				progress.style.backgroundPosition = pc + "% 0";
+			}, false);
+
+			// file received/failed
+			xhr.onreadystatechange = function(e) {
+				if (xhr.readyState == 4) {
+					progress.className = (xhr.status == 200 ? "success" : "failure");
+				}
+			};
+
+			// start upload
+			xhr.open("POST", GetElementById("upload").action, true);
+			xhr.setRequestHeader("X_FILENAME", file.name);
+			xhr.send(file);
+
+		}
+
+	}
+
+
+	// initialize
+	function Init() {
+
+		var fileselect = GetElementById("fileselect"),
+			filedrag = GetElementById("filedrag"),
+			submitbutton = GetElementById("submitbutton");
+
+		// file select
+		fileselect.addEventListener("change", FileSelectHandler, false);
+
+		// is XHR2 available?
+		var xhr = new XMLHttpRequest();
+		if (xhr.upload) {
+
+			// file drop
+			filedrag.addEventListener("dragover", FileDragHover, false);
+			filedrag.addEventListener("dragleave", FileDragHover, false);
+			filedrag.addEventListener("drop", FileSelectHandler, false);
+			filedrag.style.display = "block";
+
+			// remove submit button
+			submitbutton.style.display = "none";
+		}
+
+	}
+
+	// call initialization file
+	if (window.File && window.FileList && window.FileReader) {
+		Init();
+	}
+
+
+})();
